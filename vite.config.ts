4549b783e79a5e08f8df5346055f39bff72e016e
Aman Meherally <amanmeherally@gmail.com>
2025-09-17 18:51:15 -0400

    metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import ogPlugin from "vite-plugin-open-graph";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    ogPlugin({
+      basic: {
+        title: "AWS or Not",
+        description: "Guess if a service is from AWS or not!",
+        url: "https://aws.amanmeherally.com",
+        image: "https://aws.amanmeherally.com/og-image.png",
+        type: "website",
+        siteName: "AWS or Not",
+      },
+    }),
+  ],
   server: { allowedHosts: ["bce0785094c7.ngrok-free.app"] },
 });
